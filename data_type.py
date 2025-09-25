73ae9f10ea781f92c30e0034d3c4a279b3ed8e55
Riya Dhami <132061713+RiyaDhami13@users.noreply.github.com>
2025-09-25 12:08:22 +0545

    Create data_type.py

---
+++ data_type.py
@@ -0,0 +1,15 @@
+# Set up the exercise
+from learntools.core import binder
+binder.bind(globals())
+from learntools.intro_to_programming.ex3 import *
+print('Setup complete.')
+
+ # Define a float
+y = 1.
+print(y)
+print(type(y))
+
+# Convert float to integer with the int function
+z = int(y)
+print(z)
+print(type(z))
