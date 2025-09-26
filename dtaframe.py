5180f50f2b0f44108569b71437b5eb07b5d44525
Riya Dhami <132061713+RiyaDhami13@users.noreply.github.com>
2025-09-26 14:02:46 +0545

    Create dtaframe.py

---
+++ dtaframe.py
@@ -0,0 +1,12 @@
+
+fruits = pd.DataFrame([[30, 21]], columns=['Apples', 'Bananas'])
+
+# Check your answer
+q1.check()
+fruits
+ #given is the table 'https://storage.googleapis.com/kaggle-media/learn/images/CHPn7ZF.png'
+fruit_sales = pd.DataFrame({'Apples':[35,41],'Bananas':[21,34]}, index = ['2017 Sales','2018 Sales'])
+
+# Check your answer
+q2.check()
+fruit_sales
