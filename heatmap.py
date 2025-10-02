853f84eddd84afedee67aa4dc90c4adb274aeb24
Riya Dhami <132061713+RiyaDhami13@users.noreply.github.com>
2025-10-02 17:00:48 +0545

    Create heatmap.py

---
+++ heatmap.py
@@ -0,0 +1,8 @@
+# Set the width and height of the figure
+plt.figure(figsize=(10,10))
+# Heatmap showing average game score by platform and genre
+sns.heatmap(ign_data, annot=True)
+# Add label for horizontal axis
+plt.xlabel("Genre")
+# Add label for vertical axis
+plt.title("Average Game Score, by Platform and Genre")
