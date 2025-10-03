4ffe3223c324a23b6df0e596e5a71470938ab8e3
Riya Dhami <132061713+RiyaDhami13@users.noreply.github.com>
2025-10-03 18:46:37 +0545

    Update scatter_plot.py

--- scatter_plot.py
+++ scatter_plot.py
@@ -5,8 +5,34 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 print("Setup Complete")
 
-# Path of the file to read
-insurance_filepath = "../input/insurance.csv"
+# Set up code checking
+import os
+if not os.path.exists("../input/candy.csv"):
+    os.symlink("../input/data-for-datavis/candy.csv", "../input/candy.csv") 
+from learntools.core import binder
+binder.bind(globals())
+from learntools.data_viz_to_coder.ex4 import *
+print("Setup Complete")
 
-# Read the file into a variable insurance_data
-insurance_data = pd.read_csv(insurance_filepath)
+# Path of the file to read
+candy_filepath = "../input/candy.csv"
+
+# Fill in the line below to read the file into a variable candy_data
+candy_data = pd.read_csv(candy_filepath, index_col = 'id')
+
+# Scatter plot showing the relationship between 'sugarpercent' and 'winpercent'
+sns.scatterplot(x = ['sugarpercent'] ,y = ['winpercent'])
+
+# Scatter plot w/ regression line showing the relationship between 'sugarpercent' and 'winpercent' # Your code here
+sns.regplot(x= candy_data['sugarpercent'],y=candy_data['winpercent'])
+
+# Scatter plot showing the relationship between 'chocolate' and 'winpercent'
+sns.swarmplot(x=candy_data['chocolate'],y=candy_data['winpercent'])
+
+# Scatter plot showing the relationship between 'pricepercent', 'winpercent', and 'chocolate'
+sns.scatterplot(x = candy_data['pricepercent'], y = candy_data['winpercent'],hue = candy_data['chocolate'])
+
+# Color-coded scatter plot w/ regression lines
+sns.lmplot(x="pricepercent", y = "winpercent", hue = "chocolate", data = candy_data)
+
+
