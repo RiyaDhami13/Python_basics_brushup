7f2d5eff37a5ed6f184cd5a47a35a84c9d3c0a42
Riya Dhami <132061713+RiyaDhami13@users.noreply.github.com>
2026-02-16 17:33:40 +0545

    Implement select_second function in lists.py
    Add a function to select the second element from a list.

---
+++ lists.py
@@ -0,0 +1,15 @@
+from learntools.core import binder; binder.bind(globals())
+from learntools.python.ex4 import *
+print('Setup complete.')
+
+def select_second(L):
+    """Return the second element of the given list. If the list has no second
+    element, return None.
+    """
+    if len(L) < 2:
+        return None 
+    return L[1]
+    pass
+
+# Check your answer
+q1.check()
